3ba73dd9da137fef92493d221cf65b02df0bd74e
Vishal Singh <singhvishal2977@gmail.com>
2026-05-22 05:35:23 +0530

    build home page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,10 @@
 import type { Metadata } from 'next';
-import { Bricolage_Grotesque, Rubik } from 'next/font/google';
+import { Bricolage_Grotesque, Rubik, Geist } from 'next/font/google';
 import './globals.css';
+import { cn } from '@/lib/utils';
+import LightRays from '../components/LightRays';
+
+const geist = Geist({ subsets: ['latin'], variable: '--font-sans' });
 
 const bricolage = Bricolage_Grotesque({
   variable: '--font-bricolage',
@@ -25,9 +29,35 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`${bricolage.variable} ${rubik.variable} min-h-screen antialiased`}
+      className={cn(
+        'min-h-screen',
+        'antialiased',
+        bricolage.variable,
+        rubik.variable,
+        'font-sans',
+        geist.variable
+      )}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body>
+        <div className=" absolute inset-0 top-0 z-[-1] min-h-screen">
+          <LightRays
+            raysOrigin="top-center-offset"
+            raysColor="#5dfeca"
+            raysSpeed={0.5}
+            lightSpread={0.9}
+            rayLength={1.4}
+            followMouse={true}
+            mouseInfluence={0.02}
+            noiseAmount={0}
+            distortion={0}
+            className="custom-rays"
+            pulsating={false}
+            fadeDistance={0.01}
+            saturation={0.01}
+          />
+        </div>
+        <main>{children}</main>
+      </body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,18 @@
 import React from 'react';
 
 const page = () => {
-  return <div className="font-bricolage text-8xl">page</div>;
+  return (
+    <>
+      <section>
+        <h1 className="">
+          The hub for Every dev <br /> event you can't miss
+        </h1>
+        <p className="text-center mt-5">
+          hackathons,meetups and conferences All in one place{' '}
+        </p>
+      </section>
+    </>
+  );
 };
 
 export default page;
